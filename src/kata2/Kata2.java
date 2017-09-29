af8821dfaa0cfe2b174ce8bc5f0bfc913d78eacd
usuario <usuario@10.22.148.68>
2017-09-29 13:04:18 +0100

    Segunda versión kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -19,18 +19,13 @@ public class Kata2 {
         int [] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
         HashMap <Integer, Integer> histogram = new HashMap();
         // TODO code application logic here
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i],histogram.get(data[i])+1);
-                
-            }else{
-                histogram.put(data[i],1);
-            }
-            
+        for (int value : data) {
+            histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
         }
         for(int key:histogram.keySet()){
             System.out.println(key + " -->" + histogram.get(key)+ " veces");
         
         }
+        
     }
 }
